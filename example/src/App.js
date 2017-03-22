250ad86fe8f49c50fe047b2e3a4d030baa903120
Nguyen The Thang <justin.ntt@gmail.com>
2017-03-22 16:10:18 +0700

    working version

--- example/src/App.js
+++ example/src/App.js
@@ -2,23 +2,105 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  Alert,
 } from 'react-native';
+import { Form, Widget } from 'react-native-compose';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pickerValue: null
+    }
+  }
+
   render() {
+    const langOptions = [
+      {
+        label: '',
+        value: null,
+      },
+      {
+        label: 'English',
+        value: 'en',
+      },
+      {
+        label: '繁體字',
+        value: 'zh-HK',
+      },
+    ];
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Form
+          style={{height: 200, justifyContent: 'flex-start'}}
+          ref={ref => this.form = ref}
+          onSubmit={() => {
+            this.form.validate().then(() => {
+              Alert.alert(
+                '',
+                'All validations passed',
+                [
+                  {
+                    text: 'OK',
+                    onPress: () => { },
+                  },
+                ]
+              );
+            }).catch(result => {
+              console.log('form validation', result);
+              Alert.alert(
+                '',
+                JSON.stringify(result),
+                [
+                  {
+                    text: 'OK',
+                    onPress: () => {
+                    },
+                  },
+                ]
+              );
+            });
+          }}
+      >
+        <Widget.Picker
+          name='picker'
+          options={langOptions.map(x => {
+            return {value: x.value, label: x.label};
+          })}
+          selectedValue={this.state.pickerValue}
+          arrowDirection={'down'}
+          onValueChange={value => this.setState({pickerValue: value})}
+          validator={value => {
+            if (value == null || value == '') {
+              return 'Picker value is required';
+            }
+
+            return null;
+          }}
+          required
+        />
+        <Widget.TextInput
+          name='text'
+          underlineColorAndroid={'transparent'}
+          validator={value => {
+            if (value == null || value == '') {
+              return 'Text is required';
+            }
+
+            return null;
+          }}
+          onValueChange={(value) => {
+            console.log('onValueChange');
+          }}
+          value={'initial text'}
+          style={{ flex: 1, height: 36, padding: 2, width: 200 }}
+          keyboardType={'default'}
+        />
+        <Widget.SubmitButton title={'Submit'}/>
+      </Form>
       </View>
     );
   }
@@ -27,9 +109,10 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    paddingVertical: 50,
+    justifyContent: 'space-around',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#FFF',
   },
   welcome: {
     fontSize: 20,
--- index.js
+++
@@ -1,6 +0,0 @@
-
-import { NativeModules } from 'react-native';
-
-const { RNCompose } = NativeModules;
-
-export default RNCompose;
